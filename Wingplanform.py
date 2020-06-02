a183ee904c4b392bd18dc8fccc378089a1c0656b
Marek Travnik <marek.travnik.98@gmail.com>
2020-06-02 11:54:47 +0200

    lidar in sideview

--- Wingplanform.py
+++ Wingplanform.py
@@ -67,6 +67,10 @@ width_battery = length_pack
 length_battery = thickness_pack+ width_pack
 height_battery = 2*thickness_pack
 
+
+width_lidar =0.061
+length_lidar =0.035
+height_lidar  =0.061
 # Surface Area
 
 area = m_total *9.80665 / wing_loading
@@ -293,20 +297,26 @@ point_32 = (Data.x_CG - length_payload/2-width_pack-0.005,-width_pack/2)
 point_33 = (Data.x_CG - length_payload/2-width_pack-0.005,-thickness_pack)
 
 
-print(Data.x_CG - length_payload/2-width_pack-thickness_pack-0.005)
+#Lidar
 
+point_34 = (Data.x_CG - length_payload/2-width_pack-thickness_pack-0.01,height_lidar/2)
+point_35 = (Data.x_CG - length_payload/2-width_pack-thickness_pack-length_lidar-0.01,height_lidar/2)
+point_36 = (Data.x_CG - length_payload/2-width_pack-thickness_pack-length_lidar-0.01,-height_lidar/2)
+point_37 = (Data.x_CG - length_payload/2-width_pack-thickness_pack-0.01,-height_lidar/2)
 
 
 s_points_payload = [point_22,point_23, point_24, point_25]
 s_points_battery = [point_26,point_27, point_28, point_29,point_30,point_31,point_32,point_33]
-
+s_points_lidar  = [point_34,point_35,point_36,point_37]
 
 s_line_payload = plt.Polygon(s_points_payload,closed=True, fill=None, edgecolor='r')
 s_line_battery = plt.Polygon(s_points_battery,closed=True, fill=None, edgecolor='y')
+s_line_lidar = plt.Polygon(s_points_lidar,closed=True, fill=None, edgecolor='g')
 
 
 sideview.gca().add_line(s_line_payload)
 sideview.gca().add_line(s_line_battery)
+sideview.gca().add_line(s_line_lidar)
 ax.axis('equal')
 ax.grid(True)
 
